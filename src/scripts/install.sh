91a75ee745762f5f2f455e0c3da922d709e43efb
CubeBeveled <cubebeveled@gmail.com>
2025-06-10 14:34:17 +0200

    gonna use this to update packages ykyk

---
+++ src/scripts/install.sh
@@ -0,0 +1,2 @@
+cd ../..
+npm i @docusaurus/core @docusaurus/plugin-ideal-image @docusaurus/preset-classic @docusaurus/theme-search-algolia @mdx-js/react clsx gray-matter prism-react-renderer react react-dom
